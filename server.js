91052dff4d02352fe394b358545d4e67d6a6fe6e
danoldnyamsi7 <73864366+danoldnyamsi7@users.noreply.github.com>
2022-05-27 05:47:09 +0100

    new commit

--- server.js
+++ server.js
@@ -11,7 +11,61 @@ const { db_conn } = require("./api-v-1.0/configurations/database");
 // middlewares
 app.use(cors());
 app.use(express.json({limit:'100mb'}));
-app.use(helmet());
+app.use(
+    helmet.contentSecurityPolicy({
+      useDefaults: false,
+      "block-all-mixed-content": true,
+      "upgrade-insecure-requests": true,
+      directives: {
+        "default-src": [
+            "'self'"
+        ],
+        "base-uri": "'self'",
+        "font-src": [
+            "'self'",
+            "https:",
+            "data:"
+        ],
+        "frame-ancestors": [
+            "'self'"
+        ],
+        "img-src": [
+            "'self'",
+            "data:"
+        ],
+        "object-src": [
+            "'none'"
+        ],
+        "script-src": [
+            "'self'",
+            "https://cdnjs.cloudflare.com"
+        ],
+        "script-src-attr": "'none'",
+        "style-src": [
+            "'self'",
+            "https://cdnjs.cloudflare.com"
+        ],
+      },
+    }),
+    helmet.dnsPrefetchControl({
+        allow: true
+    }),
+    helmet.frameguard({
+        action: "deny"
+    }),
+    helmet.hidePoweredBy(),
+    helmet.hsts({
+        maxAge: 123456,
+        includeSubDomains: false
+    }),
+    helmet.ieNoOpen(),
+    helmet.noSniff(),
+    helmet.referrerPolicy({
+        policy: [ "origin", "unsafe-url" ]
+    }),
+    helmet.xssFilter()
+)
+
 
 // connect to mongo atlas
 db_conn();
